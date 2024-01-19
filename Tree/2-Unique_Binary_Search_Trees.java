cc677b388ad7786cb5a9d8b2c213e45dea46ea92
Om Singh <omsingh32123@gmail.com>
2024-01-20 01:14:29 +0530

    2 : Unique Binary Search Trees

---
+++ Tree/2-Unique_Binary_Search_Trees.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numTrees(int n) {
+        int array[] = new int[n+1];
+        array[0] = 1;
+        array[1] = 1;
+        for(int i=2;i<=n;i++){
+            int x=0;
+            int y = i-1;
+            int sum=0;
+            while(x<i&&y>=0) {
+                sum+=array[x]*array[y];
+                x++;
+                y--;
+            }
+            array[i] = sum;
+        }
+        return array[n];
+    }
+}
