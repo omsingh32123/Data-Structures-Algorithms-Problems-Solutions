617a0bbdb636e1883de50e7274b514ccccaf6aa7
Om Singh <omsingh32123@gmail.com>
2024-02-28 13:26:18 +0530

    2 : Fibonacci Number

--- Dynamic Programming/2-Fibonacci_Number.java
+++ Dynamic Programming/2-Fibonacci_Number.java
@@ -20,3 +20,14 @@ class Solution {
         return num;
     }
 }
+
+// Non-DP approach || Simple Recursion approach
+class Solution {
+    public int fib(int n) {
+        if(n==0)
+        return 0;
+        if(n==1)
+        return 1;
+        return fib(n-2)+fib(n-1);
+    }
+}
