d649f96e1b7c255c9a8ad6730e49956db6e139a1
Om Singh <omsingh32123@gmail.com>
2024-02-28 13:23:20 +0530

    2 : Fibonacci Number

---
+++ Dynamic Programming/2-Fibonacci_Number.java
@@ -0,0 +1,22 @@
+// Question Name : Fibonacci Number
+// Question URL : https://leetcode.com/problems/fibonacci-number/
+
+import java.util.*;
+class Solution {
+    public int fib(int n) {
+        HashMap<Integer, Integer> map=new HashMap<>();
+        int num=fibo(n,map);
+        return num;
+    }
+    public int fibo(int n, HashMap<Integer, Integer> map) {
+        if(map.containsKey(n))
+        return map.get(n);
+        if(n==0)
+        return 0;
+        if(n==1)
+        return 1;
+        int num = fibo(n-2,map)+fibo(n-1,map);
+        map.put(n,num);
+        return num;
+    }
+}
