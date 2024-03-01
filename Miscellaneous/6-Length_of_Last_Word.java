417a45690cbae6feaaf6d37a3e676c9c723996b9
Om Singh <omsingh32123@gmail.com>
2024-03-01 23:53:20 +0530

    6 : Added approach 2 of Question

--- Miscellaneous/6-Length_of_Last_Word.java
+++ Miscellaneous/6-Length_of_Last_Word.java
@@ -1,10 +1,20 @@
 // Question Name : Length of Last Word
 // Question URL : https://leetcode.com/problems/length-of-last-word/
 
+// Approach 1
 class Solution {
     public int lengthOfLastWord(String s) {
         String sr[]=s.split(" ");
         int x=sr[sr.length-1].length();
         return x;
+    }
+}
+
+// Approach 2
+class Solution {
+    public int lengthOfLastWord(String s) {
+        s=s.trim();
+        int x=s.lastIndexOf(" ");
+        return s.length()-x-1;
     }
 }
