fd2b1686bfb83703610398b50cac126c2d82f5ec
Om Singh <omsingh32123@gmail.com>
2024-03-27 18:44:26 +0530

    29 : Subarray Product Less Than K

---
+++ Miscellaneous/29-Subarray_Product_Less_Than_K.java
@@ -0,0 +1,20 @@
+// Question Name : Subarray Product Less Than K
+// Question URL : https://leetcode.com/problems/subarray-product-less-than-k/
+
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        if (k <= 1) return 0;
+
+        int left = 0, right = 0, product = 1, count = 0;
+        int n = nums.length;
+
+        while (right < n) {
+            product *= nums[right];
+            while (product >= k) product /= nums[left++];
+            count += 1 + (right - left);
+            right++;
+        }
+
+        return count;
+    }
+}
