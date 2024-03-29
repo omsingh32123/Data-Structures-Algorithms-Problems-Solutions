19484a3df75e91383bb1a9fa4f4d34052d04abdd
Om Singh <omsingh32123@gmail.com>
2024-03-29 18:17:39 +0530

    31 : Count Subarrays Where Max Element Appears at Least K Times

---
+++ Miscellaneous/31-Count_Subarrays_Where_Max_Element_Appears_at_Least_K_Times.java
@@ -0,0 +1,30 @@
+// Question Name : Count Subarrays Where Max Element Appears at Least K Times
+// Question URL : https://leetcode.com/problems/count-subarrays-where-max-element-appears-at-least-k-times/
+
+class Solution 
+{
+    public long countSubarrays(int[] ar, int k) 
+    {
+        long n=ar.length,max=0;
+        for(int i=0;i<n;i++)
+        {
+            if(max<ar[i])
+            max=ar[i];
+        }
+        long l=0,r=0,count=0,ans=0;
+        while( r<ar.length || l > r )
+        {
+            if(ar[(int)r]==max)
+            count++;
+            while( count>=k )
+            {
+                if(ar[(int)l]==max)
+                    count--;
+                l++;
+                ans += ar.length-r;
+            }
+            r++;
+        }
+        return ans;
+    }
+}
