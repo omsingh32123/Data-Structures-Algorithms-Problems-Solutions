6655b160bbc0bdf9e3da75e0104c6380445df62b
Om Singh <omsingh32123@gmail.com>
2024-04-07 23:21:39 +0530

    4 : Valid Parenthesis String

---
+++ String/4-Valid_Parenthesis_String.java
@@ -0,0 +1,21 @@
+// Question Name : Valid Parenthesis String
+// Question URL : https://leetcode.com/problems/valid-parenthesis-string/
+
+class Solution {
+    public boolean checkValidString(String s) {
+        int leftCount = 0, rightCount = 0;
+        
+        for (char c : s.toCharArray()) {
+            leftCount += c == '(' ? 1 : -1;
+            rightCount += c == ')' ? -1 : 1;
+            
+            if (rightCount < 0) {
+                break;
+            }
+            
+            leftCount = Math.max(leftCount, 0);
+        }
+
+        return leftCount == 0;
+    }
+}
