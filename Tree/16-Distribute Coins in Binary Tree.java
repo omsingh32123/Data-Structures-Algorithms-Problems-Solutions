a4c972ee035b6304ece88182d1ccf65d27ad41d1
Om Singh <omsingh32123@gmail.com>
2024-05-18 12:21:03 +0530

    16 : Distribute Coins in Binary Tree

---
+++ Tree/16-Distribute Coins in Binary Tree.java
@@ -0,0 +1,31 @@
+// Question Name :  Distribute Coins in Binary Tree
+// Question URL : https://leetcode.com/problems/distribute-coins-in-binary-tree/
+
+class Solution {
+    public int ans;
+    public int distributeCoins(TreeNode root) {
+        ans = 0;
+        countSteps(root);
+        return ans;
+    }
+    public int countSteps(TreeNode root) {
+        if (root == null) return 0;
+        int leftCoins = countSteps(root.left);
+        int rightCoins = countSteps(root.right);
+        ans += Math.abs(leftCoins) + Math.abs(rightCoins);
+        return (root.val - 1) + leftCoins + rightCoins;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
