d57dfbb8455a7c09f665fcfe8215b9f63f178aed
Om Singh <omsingh32123@gmail.com>
2024-06-12 18:54:31 +0530

    58 : Contains Duplicate II

---
+++ Miscellaneous/58-Contains Duplicate II.java
@@ -0,0 +1,29 @@
+// Question Name :  Contains Duplicate II
+// Question URL : https://leetcode.com/problems/contains-duplicate-ii/
+
+import java.util.HashSet;
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashSet<Integer> set = new HashSet<>();
+        int minInd = Math.min(nums.length,k);
+        for( int i=0 ; i<minInd ; i++ )
+        {
+            set.add(nums[i]);
+        }
+        if( set.size() < minInd )
+        {
+            return true;
+        }
+        for( int i=minInd ; i<nums.length ; i++ )
+        {
+            set.add( nums[i] );
+            if( set.size() < minInd+1 )
+            {
+                return true;
+            }
+            set.remove( nums[i-minInd] );
+        }
+        return false;
+    }
+}
