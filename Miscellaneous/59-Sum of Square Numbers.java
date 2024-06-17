a6a8fc90827a1b462e1f39d929aa9fec28a91d85
Om Singh <omsingh32123@gmail.com>
2024-06-17 23:01:59 +0530

    59 : Sum of Square Numbers

---
+++ Miscellaneous/59-Sum of Square Numbers.java
@@ -0,0 +1,20 @@
+// Question Name :  Sum of Square Numbers
+// Question URL : https://leetcode.com/problems/sum-of-square-numbers/description/
+
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        for (int divisor = 2; divisor * divisor <= c; divisor++) {
+            if (c % divisor == 0) {
+                int exponentCount = 0;
+                while (c % divisor == 0) {
+                    exponentCount++;
+                    c /= divisor;
+                }
+                if (divisor % 4 == 3 && exponentCount % 2 != 0) {
+                    return false;
+                }
+            }
+        }
+        return c % 4 != 3;
+    }
+}
